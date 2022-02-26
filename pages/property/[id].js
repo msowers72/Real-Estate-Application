75831153c6367ca220eeb8e29eef260e07d42cee
Mike Sowers <msowers72@yahoo.com>
2022-02-26 13:31:32 -0500

    added scroll feature

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -5,10 +5,16 @@ import { GoVerified } from 'react-icons/go'
 import millify from 'millify'
 
 import { baseUrl, fetchApi} from '../../utils/fetchApi'
+import ImageScrollbar from '../../components/ImageScrollbar';
 
 const PropertyDetails = ( {propertyDetails: { price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, amenities, photos }} ) => (
     <Box maxWidth="1000px" margin="auto" p="4">
         {photos && <ImageScrollbar data={photos} />}
+        <Box w="full" p="6">
+            <Flex paddingTop="2" alignItems="center">
+                
+            </Flex>
+        </Box>
     </Box>
 )
 
@@ -19,7 +25,7 @@ export async function getServerSideProps( {params: { id }}) {
 
     return{
         props: {
-            propertyDetails: data
+            propertyDetails: data,
         }
     }
 }
